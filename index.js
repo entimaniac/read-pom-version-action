8a58b7f866af750d9b1cfbfb99b82ee7ef4d727f
Jesse Strode <jesse_strode@condenast.com>
2021-02-02 15:42:53 -0500

    clean up for publishing

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ const pomParser = require("pom-parser");
 
 try {
   let opts = {
-    filePath: __dirname + "/pom.xml",
+    filePath: process.env.GITHUB_WORKSPACE + "/pom.xml",
   };
-  pomParser.parse(opts, function(error, pomResponse) {
-    console.log("Reading pom.xml from project root")
+  pomParser.parse(opts, function (error, pomResponse) {
+    console.log("parsing pom.xml from project root");
 
     if (error) {
-      console.log("an error occurred")
+      console.log("an error occurred");
       core.setFailed(error.message);
       process.exit(1);
     }
 
-    console.log("Successfully parsed pom.xml ")
+    console.log("Successfully parsed pom.xml ");
     core.setOutput("version", pomResponse.pomObject.project.version);
-    console.log("project version is: ", pomResponse.pomObject.project.version)
+    console.log("project version is: ", pomResponse.pomObject.project.version);
   });
-
 } catch (error) {
   core.setFailed(error.message);
 }
